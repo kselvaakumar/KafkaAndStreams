5e382f402a2c70090fbb5fbe145bd8ce99f7e4b2
Selvakumar Kumaresan <selvakumar.kumaresan@target.com>
2022-05-24 20:33:22 +0530

    initial

---
+++ src/main/java/com/example/kafkaAndStreams/KafkaAndStreamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaAndStreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAndStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAndStreamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaAndStreams/KafkaAndStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaAndStreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAndStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
